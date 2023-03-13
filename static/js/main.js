d8dbcddce857d3897ccf5452c6150ae7be5a2b76
Artem <artnik349@gmail.com>
2023-03-13 21:15:51 +0200

    final version of markup

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,10 @@
 function show_popap(id_popap) {
-    var id = "#" + id_popap;
-    $(id).addClass('active');
-  }
-   
-  // функция закрытия окна 
-  $(".close_popap").click( function(){
-    $(".overlay").removeClass("active");
-  });
-
-  let i = 1;
-  for(let li of carousel.querySelectorAll('li')) {
-    li.style.position = 'relative';
-    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0"></span>`);
-    i++;
-  }
+  $(".overlay").removeClass("active");
+  var id = "#" + id_popap;
+  $(id).addClass('active');
+}
+ 
+// функция закрытия окна 
+$(".close_popap").click( function(){
+  $(".overlay").removeClass("active");
+});
